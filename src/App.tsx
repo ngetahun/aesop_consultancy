f74f0b542a2604d7e27ded06a93b4ff76ea9439d
Natnael Getahun <natnael.getahun@suse.com>
2024-12-07 19:58:01 +0100

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <Analytics />
       <Navbar />
       <Hero />
       <Services />
